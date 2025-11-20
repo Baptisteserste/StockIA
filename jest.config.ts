a4b1efcd226a69ebc84e8d407e638b67cb1ad280
Baptiste <baptiste.serste@gmail.com>
2025-11-20 20:55:46 +0100

    Ajout des tests

--- jest.config.ts
+++ jest.config.ts
@@ -2,20 +2,18 @@ import type { Config } from 'jest'
 import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
-    // Chemin vers votre app Next.js pour charger next.config.js et .env
     dir: './',
 })
 
-// Add any custom config to be passed to Jest
 const config: Config = {
     coverageProvider: 'v8',
     testEnvironment: 'jsdom',
-    // Ajoute des matchers comme .toBeInTheDocument()
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     moduleNameMapper: {
-        // Gérer les alias de chemin (@/...)
         '^@/(.*)$': '<rootDir>/$1',
-    }
+    },
+
+    transformIgnorePatterns: [],
 }
 
 export default createJestConfig(config)
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,25 @@
 import '@testing-library/jest-dom'
+import { TextEncoder, TextDecoder } from 'util'
+
+global.TextEncoder = TextEncoder
+// @ts-expect-error - Nécessaire car les signatures TS ne matchent pas exactement pour JSDOM
+global.TextDecoder = TextDecoder
+
+// Mock complet de Crypto pour Clerk
+if (typeof global.crypto === 'undefined') {
+    Object.defineProperty(global, 'crypto', {
+        value: {
+            subtle: {
+                // .buffer est important ici pour satisfaire TypeScript (ArrayBuffer)
+                digest: () => Promise.resolve(new Uint8Array(32).buffer),
+            },
+            getRandomValues: (arr: Uint8Array) => {
+                // Simulation simple de nombres aléatoires
+                for (let i = 0; i < arr.length; i++) {
+                    arr[i] = Math.floor(Math.random() * 256);
+                }
+                return arr;
+            }
+        }
+    });
+}
